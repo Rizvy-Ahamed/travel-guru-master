a5d32645018d280fe3712a487b1ef8c5f68e21c8
Rizvy-Ahamed <rizvyahamed97@gmail.com>
2020-09-25 03:03:05 +0600

    Assignment-9

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { createContext, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header/Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Banner from './Components/Banner/Banner';
+import Booking from './Components/Booking/Booking';
+import Login from './Components/Login/Login';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+import Destination from './Components/Destination/Destination';
+import NotFound from './Components/NotFound/NotFound';
+import Blog from './Components/Blog/Blog';
+import Contact from './Components/Contact/Contact';
+
+export const UserContext = createContext()
+function App() {
+  const [loggedInUser , setLoggedInUser]= useState({
+        // isSignIn : false,
+        name :'',
+        emails:'',
+        photo :'',
+        password :'',
+        message :'',
+  }) 
+  return (
+  <UserContext.Provider value={[loggedInUser,setLoggedInUser]}>
+    <Router> 
+      <Switch>
+          <Route path='/home'>
+              <Banner></Banner>
+          </Route>
+          <Route path='/news'>
+              <Banner></Banner>
+          </Route>
+          <Route path='/booking/:Id'>
+              <Booking></Booking>
+          </Route>
+          <PrivateRoute path='/destination'>
+              <Destination/>
+          </PrivateRoute>
+          <Route path='/login'>
+              <Login></Login>
+          </Route>
+          <Route path='/blog'>
+              <Blog/>
+          </Route>
+          <Route path='/contact'>
+              <Contact/>
+          </Route>
+          <Route exact path='/'>
+              <Banner></Banner>
+          </Route>
+          <Route  path='*'>
+              <NotFound/>
+          </Route>
+      </Switch>
+    </Router>
+  </UserContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/firebaseConfig.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyApQxWHAJ43Qd5TWUWCzdI8A_fDc0fb3l4",
+  authDomain: "travel-guru-27160.firebaseapp.com",
+  databaseURL: "https://travel-guru-27160.firebaseio.com",
+  projectId: "travel-guru-27160",
+  storageBucket: "travel-guru-27160.appspot.com",
+  messagingSenderId: "1077660569367",
+  appId: "1:1077660569367:web:f5299cd68f7566d488b152"
+};
+  export default firebaseConfig;
